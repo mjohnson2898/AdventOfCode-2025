4c786ae3aa2afc28375c781cf2d2301e2804c26f
mrj9235 <mrj9235@g.rit.edu>
2025-12-05 18:42:52 -0600

    Part one started part 2

---
+++ Day4/part2.py
@@ -0,0 +1,50 @@
+def main():
+    file = open("Day4/input.txt", "r")
+    lines = []
+    for line in file:
+        lines.append(line.strip())
+    file.close()
+    findSurrounding(lines)
+
+def findSurrounding(lines):
+    total = 0
+    i = 0
+    while i < len(lines):
+        j = 0
+        currLine = lines[i]
+        while j < len(currLine):
+            surrounding = 0
+            if currLine[j] == ".":
+                j+=1
+            else:
+                if i > 0:
+                    if j > 0 and lines[i-1][j-1] == "@":
+                        surrounding += 1
+                    if j < len(currLine)-1 and lines[i-1][j+1]=="@":
+                        surrounding += 1
+                    if lines[i-1][j] == "@":
+                        surrounding += 1
+                if j > 0:
+                    if currLine[j-1] == "@":
+                        surrounding += 1
+                if j < len(currLine)-1:
+                    if currLine[j+1] == "@":
+                        surrounding += 1
+                if i < len(lines)-1:
+                    if j > 0 and lines[i+1][j-1] == "@":
+                        surrounding += 1
+                    if j < len(currLine)-1 and lines[i+1][j+1]=="@":
+                        surrounding += 1
+                    if lines[i+1][j] == "@":
+                        surrounding += 1
+                if surrounding < 4:
+                    total += 1
+                    lines[i][j] = '.'
+                j+=1
+        i+=1
+    print(total)
+    while total > 0:
+        findSurrounding(lines)
+            
+if __name__ == "__main__":
+    main()
